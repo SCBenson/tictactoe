f65efd0bf2bd2c69d89168f951e61bd8c572c080
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2024-03-12 09:43:32 +0000

    working on the script file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const cells = document.querySelectorAll(".board-item")
 const openPlayerChoice = document.querySelector("#playerMovePrompt")
 
+const p1Score = document.getElementById("#p1-score")
+const p2Score = document.getElementById("#p2-score")
 
+
+//---Increment the score when a player wins---//
+function addScore(score){
+    score++;
+    score.innerHTML = score;
+}
 // function createGame(){
 
 // }
